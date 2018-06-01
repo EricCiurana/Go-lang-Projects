581bc0a9564344a09817e9275b953ff4a3f80447
Eric <kboxercillo@gmail.com>
2018-06-02 01:11:55 +0200

    Hello World

---
+++ Hello World/main.go
@@ -0,0 +1,13 @@
+/*
+	Author: 			Eric Ciurana
+	Date(dd/mm/yyyy):	01/06/2018
+	License:			This is free software dude, use it as you want!
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
