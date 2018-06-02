553446fa12a70ef2fec7f53944de3a858a03b136
Eric <kboxercillo@gmail.com>
2018-06-02 22:45:03 +0200

    Finished with Structs

--- Structs/main.go
+++ Structs/main.go
@@ -10,22 +10,48 @@ import (
 	"fmt"
 )
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
-	subject001 := person{"John", "Doe"}
+
+	subject001 := person{"John", "Doe", contactInfo{"john.doe@internet.com", 31415}}
 	fmt.Println(subject001)
 
-	subject002 := person{firstName: "Jane", lastName: "Doe"}
-	fmt.Println(subject002)
+	subject002 := person{
+		firstName: "Jane",
+		lastName:  "Doe",
+		contactInfo: contactInfo{
+			email:   "jane.doe@internet.com",
+			zipCode: 11235,
+		},
+	}
+	fmt.Printf("%+v", subject002)
 
 	var subject003 person
-	fmt.Println(subject003)         // Shall print '{ }'
-	fmt.Printf("%+v\n", subject003) // Shall print '{firstName: lastName: }'
+	fmt.Println(subject003)         // Shall print '{ { 0}}'
+	fmt.Printf("%+v\n", subject003) // Shall print '{firstName: lastName: contact:{ email: zipCode:0}}'
 	subject003.firstName = "Eric"
 	subject003.lastName = "Ciurana"
-	fmt.Println(subject003) // Shall print '{Eric Ciurana}'
+	//fmt.Println(subject003) // Shall print '{Eric Ciurana { 0}}'
+	subject003.print()
+
+	subject003.updateName("Erica")
+	subject003.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
+
+func (p *person) updateName(newFirstName string) {
+	(*p).firstName = newFirstName
 }
