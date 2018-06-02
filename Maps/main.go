a7a7b90ea5d38180fe62a1f1c77a6d491103dca9
Eric <kboxercillo@gmail.com>
2018-06-02 23:45:04 +0200

    Finished with Maps

---
+++ Maps/main.go
@@ -0,0 +1,44 @@
+/*
+	Author: 			Eric Ciurana
+	Date(dd/mm/yyyy):	01/06/2018
+	License:			This is free software dude, use it as you want!
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	colours := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"blue":  "#0000ff",
+	}
+	fmt.Println(colours)
+
+	/* Creation of empty maps */
+	var names map[string]string
+	fmt.Println(names)
+
+	projects := make(map[string]string)
+	fmt.Println(projects)
+
+	/* Filling/reasigning maps */
+	projects["WebApp"] = "TODO"
+	projects["Crash Sensor"] = "FINISHED"
+	fmt.Println(projects)
+	projects["WebApp"] = "FINISHED"
+	fmt.Println(projects)
+
+	/* Deleting map entries */
+	delete(projects, "WebApp")
+	fmt.Println(projects)
+
+	fmt.Println()
+	/* Iterating over maps */
+	for colour, hex := range colours {
+		fmt.Println(colour + " = " + hex)
+	}
+}
